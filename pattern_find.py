f05ad793c841952e5236ecd38916338439026586
GeorgeLuo <georgeluo@Georges-Mini.fios-router.home>
2017-02-16 04:50:21 -0500

    some basic methods

--- pattern_find.py
+++ pattern_find.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import math
+
 chg = {"startIndex" : 0, "endIndex" : 4, "sequence" : 2, "bytes" : ['111', '101', '111', '011', '001']}
 
 root = ['','','','','','','','']
@@ -45,7 +48,7 @@ def testWindows(maxSeqLength, byteIndexes):
 		for begIndex in range(0, len(byteIndexes)):
 			for j in range(begIndex + 2, len(byteIndexes) + 1):
 				if isSequence(i, byteIndexes[begIndex:j]) and not ((start <= begIndex) and (end >= j)):
-					print byteIndexes[begIndex:j]
+					print (byteIndexes[begIndex:j])
 					if last_found != None:
 						if byteIndexes[begIndex:(j - 1)] == last_found:
 							ret.pop()
@@ -55,8 +58,96 @@ def testWindows(maxSeqLength, byteIndexes):
 					last_found = byteIndexes[begIndex:j]
 	return ret
 
-print (genSequence(chg, root))
-print(testWindows(5, mem))
+# returns array of substring permutations of input string
+from itertools import permutations
+def get_substrings(input_text):
+	return set([''.join(p) for p in permutations(input_text)])
+
+# return x by y matrix with default_data in every location
+def x_by_y_matrix(x, y, default_data):
+	return [[default_data for a in xrange(0,x)] for b in xrange(0,y)]
+
+# print(x_by_y_matrix(4,5,'hello'))
+# print (get_substrings('quandary'))
+# print (genSequence(chg, root))
+# print(testWindows(5, mem))
+
+import random
+
+# pow(x,y,z) = (x^y) % z
+
+# use Python 3 print function
+# this allows this code to run on python 2.x and 3.x
+
+from Crypto.Util import number
+
+n_length = 2048
+
+ 
+# Variables Used
+
+
+sharedPrime = int('0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1'
+         '29024E088A67CC74020BBEA63B139B22514A08798E3404DD'
+         'EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245'
+         'E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED'
+         'EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381'
+         'FFFFFFFFFFFFFFFF', 0)
+
+# sharedPrime = int('0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67'
+#             'CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF2'
+#             '5F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6'
+#             'F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007C'
+#             'B8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62'
+#             'F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32'
+#             '905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9'
+#             'DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC4'
+#             '2DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D06'
+#             '0C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261A'
+#             'D2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE11757'
+#             '7A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D1'
+#             '20A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A94'
+#             '6834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA628'
+#             '7C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AF'
+#             'B81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C0'
+#             '8F4DF435C93402849236C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA'
+#             '37BDF8FF9406AD9E530EE5DB382F413001AEB06A53ED9027D831179727B086'
+#             '5A8918DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B33205151'
+#             '2BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03F482D7'
+#             'CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97FBEC7E8F323A9'
+#             '7A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AACC8F6D7EBF48E1D814'
+#             'CC5ED20F8037E0A79715EEF29BE32806A1D58BB7C5DA76F550AA3D8A1FBFF0'
+#             'EB19CCB1A313D55CDA56C9EC2EF29632387FE8D76E3C0468043E8F663F4860'
+#             'EE12BF2D5B0B7474D6E694F91E6DCC4024FFFFFFFFFFFFFFFF', 0)
+
+sharedBase = 2      # g
+ 
+aliceSecret = 32     # a
+bobSecret = 54      # b
+ 
+# Begin
+print( 'Publicly Shared Variables:')
+print( 'Publicly Shared Prime: ', sharedPrime)
+print( 'Publicly Shared Base:  ', sharedBase )
+ 
+# Alice Sends Bob A = g^a mod p
+A = pow(sharedBase, aliceSecret, sharedPrime) # (sharedBase**aliceSecret) % sharedPrime
+print( 'Alice Sends Over Public Chanel: ', A )
+ 
+# Bob Sends Alice B = g^b mod p
+B = pow(sharedBase, bobSecret, sharedPrime)
+print('Bob Sends Over Public Chanel: ', B )
+ 
+print( 'Privately Calculated Shared Secret: ' )
+# Alice Computes Shared Secret: s = B^a mod p
+aliceSharedSecret = pow(B, aliceSecret, sharedPrime)
+print( 'Alice Shared Secret: ', aliceSharedSecret )
+ 
+# Bob Computes Shared Secret: s = A^b mod p
+bobSharedSecret = pow(A, bobSecret, sharedPrime)
+print( 'Bob Shared Secret: ', bobSharedSecret )
+
+
 
 
 
